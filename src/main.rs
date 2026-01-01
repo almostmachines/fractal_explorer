9e6b1310bc0b518ed36393de56aa861d5d8a82eb
John King <jking@almostmachines.dev>
2026-01-01 23:03:50 +0800

    Test main()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     fractal_explorer::mandelbrot_controller()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_main_returns_ok() {
+        let result = main();
+
+        assert!(result.is_ok());
+    }
+}
