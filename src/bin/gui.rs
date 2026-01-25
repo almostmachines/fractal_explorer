e5ff0d4237fa13c72b961df0bdc3c54e69b737cf
John King <jking@almostmachines.dev>
2026-01-25 17:00:05 +0800

    Line break

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -1,5 +1,6 @@
 fn main() {
     let presenter_factory = fractal_explorer::factory::PixelsPresenterFactory {};
     let command = fractal_explorer::gui_command::GuiCommand::new(presenter_factory);
+
     command.run();
 }
